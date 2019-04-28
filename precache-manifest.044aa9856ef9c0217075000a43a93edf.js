7dd8a9ea879f716afda3beedeaf011e341ca18c2
angad <angad88@hotmail.com>
2019-04-28 07:20:39 -0400

    Updates

---
+++ precache-manifest.044aa9856ef9c0217075000a43a93edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417e685994712f9e4cc2a433223fcb26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "438fc323ce8e984f127a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "34775bafb9604bc6cd06",
+    "url": "/static/css/main.2186e73b.chunk.css"
+  },
+  {
+    "revision": "438fc323ce8e984f127a",
+    "url": "/static/js/2.cdcc9129.chunk.js"
+  },
+  {
+    "revision": "34775bafb9604bc6cd06",
+    "url": "/static/js/main.3d535549.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
