72b451e5d31ff810247502980fd94f13741f13a1
Angad Singh <angad88@hotmail.com>
2019-06-03 09:52:28 -0400

    Updated Work Experience
    Added more details to the work experience of Evertz and TCS

--- src/scripts/Work.js
+++ src/scripts/Work.js
@@ -21,6 +21,10 @@ class Work extends Component {
                         Triaging and fixing issues arise at RBC Bank’s Anti Money Laundering system. Created SQL queries to help find causes of issues. 
                     </li> 
                     <li> 
+                        Created scripts in Batch, Python, Shell to help automate daily tasts. This includes execution of SQL queries and the creation of Excel 
+                        spreed sheets detailing issues of irregularities in the systems. 
+                    </li>
+                    <li> 
                         Preformed upgrades and changes on the live Linux based production environment.   
                     </li>
                 </ul>
@@ -34,6 +38,10 @@ class Work extends Component {
                     <li> 
                         Created scripts to automate the prepossessing and transfer of assets using JavaScript and Python. Git was used as the primary version control manager for scripts
                     </li> 
+                    <li>
+                        Created the primary script for Non Linear Delivery system in the NBCU project. This system would deliver assets to multiple different platforms with 
+                        different transfer types including Signiant and Faspex. 
+                    </li>
                     <li> 
                         Created SQL queries that would help users search for different assets in the UI. Users would be able to provide different parameters for the search. 
                     </li>
@@ -42,9 +50,6 @@ class Work extends Component {
                     </li>
                     <li> 
                         Utilized agile work methodologies to ensure that scripts can be changed easily to meet customers’ requirements. Attended daily scrum meetings to collaborate with team members and inform them of any issues.  
-                    </li> 
-                    <li> 
-                        Documented detailing steps required for changes on live systems. Created doc strings and comments in source code to help understand usage of functions. 
                     </li> 
                     <li> 
                         Participated in bi weekly sprint meetings to organize workload for the upcoming sprint. Used JIRA rank tickets based on their difficult and time required.  
