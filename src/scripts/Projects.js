a177594093fa5eeced680eab1dc9ba379d2da198
angad <angad88@hotmail.com>
2019-08-17 16:51:54 -0400

    Updated all descriptions of work and projects

--- src/scripts/Projects.js
+++ src/scripts/Projects.js
@@ -14,72 +14,63 @@ class Projects extends Component {
                 <p className="header" id="Projects">Academic Projects</p>
                 <div className="card card-style" >
                     <nav className="navbar">
-                        <h5 className="card-title"> 
-                            <a href="https://github.com/singha95/RiffWarrior">Riff Warrior – C#, Unity </a> 
+                        <h5 className="card-title">
+                            <a href="https://github.com/singha95/RiffWarrior">Riff Warrior – C#, Unity </a>
                         </h5>
                         <h5 className="card-title"> Sept 2017 - April 2018 </h5>
                     </nav>
                     <ul>
-                        <li>
-                            Worked with a diverse team of programmers, artists and musicians to create a third person action adventure game in the Unity game engine. The game utilized the rock band controller for player movement and the battle system. The game was showcased at the Level-Up 2018, student games showcase, at the Design Exchange in Toronto
-                </li>
-                        <li>
-                            Players would explore a maze to search for an exit. At the end there would be a final boss for players to defeat, which would be similar to a rock band song.
-                </li>
-                        <li>
-                            Created a tutorial for players to get accustomed to the game controls. In addition it would introduce players to the rhythm based battle mechanics.
-                </li>
-                        <li>
-                            Performed user testing with other students to find game bugs and to receive feedback in order to improve gameplay.
-                </li>
+                        <li> Cooperated with a diverse team of programmers, artists and musicians to create a third person action adventure game in the Unity game engine.</li>
+                        <li> Players would explore a maze to search for an exit. At the end there would be a final boss for players to defeat, which would be similar to a rock band song. </li>
+                        <li> Improved notes sync with music and improved accuracy of input detection so that the battle system was more responsive and matched the music that was playing.</li>
+                        <li> Created a tutorial for players to get accustomed to the game controls and to introduce players to the rhythm based battle mechanics. </li>
+                        <li> Performed user testing with other students to find game bugs and to receive feedback in order to improve gameplay and to find bugs in the game. </li>
+                        <li> Utilized the rock band controller for player movement and the battle system. The game was showcased at the Level-Up 2018, student games showcase, at the Design Exchange in Toronto</li>
                     </ul>
                 </div>
 
                 <div className="card card-style" >
                     <nav className="navbar">
-                        <h5 className="card-title"> 
-                            <a href="https://github.com/Richmond-Truong/groupFinder"> 
-                                Group Finder – JavaScript, React, Node JS 
+                        <h5 className="card-title">
+                            <a href="https://github.com/Richmond-Truong/groupFinder">
+                                Group Finder – JavaScript, React, Node JS
                             </a>
                         </h5>
                         <h5 className="card-title">  May 2018 – Sep 2018</h5>
                     </nav>
                     <ul>
-                        <li>
-                            Worked with peers to create a tool that would help students post their personal projects in order to find like minded team members.
-                    </li>
-                        <li>
-                            Created a Mongo database to hold user data and theirs posts. Ensured that the web page was correctly pulling and posting data to and from the Mongo database.
-                    </li>
-                        <li>
-                            Developed front end UI using React for posting and viewing profiles. Collaborated with team members to improve the usability for the interface.
-                    </li>
+                        <li> Collaborated peers to create a tool that would help students post their personal projects in order to find like-minded team members.  </li>
+                        <li> Created a Heroku  PaaS to hold user data and theirs posts. Ensured that the web page was correctly pulling and posting data to and from Heroku .</li>
+                        <li> Developed front end UI using React for posting and viewing profiles. Collaborated with team members to improve the usability for the interface. </li>
+                        <li> Preformed user testing to ensure that all features were working and to receive feedback to help make the UI more engaging and user-friendly. eveloped 
+                            front end UI using React for posting and viewing profiles. Collaborated with team members to improve the usability for the interface. 
+                        </li>
                     </ul>
-                </div>
-
-                <div className="card card-style" >
-                    <nav className="navbar">
-                        <h5 className="card-title">  
-                            <a href="https://github.com/singha95/CSPSolver">
-                                CSP Problem Solver - C# 
-                            </a> 
-                        </h5>
-                        <h5 className="card-title"> July 2017 – Sept 2017 </h5>
-                    </nav>
-                    <ul>
-                        <li>
-                            Created a constraint satisfaction problem solver in Python. The program takes variables and theirs constraints. The program will provide a valid assignment for each of the variables within the given constraints.
-                    </li>
-                        <li>
-                            Implemented a Nonogram puzzle in order to test the performance and correctness of the program. The result of the puzzle would be displayed in a UI using the Tkinter Python API
-                    </li>
-                        <li>
-                            Converted the program into C# and improved memory efficiency using lambda functions for the constraints.
-                    </li>
-                    </ul>
-                </div>
-
             </div>
+
+            <div className="card card-style" >
+                <nav className="navbar">
+                    <h5 className="card-title">
+                        <a href="https://github.com/singha95/CSPSolver">
+                            CSP Problem Solver - C#
+                            </a>
+                    </h5>
+                    <h5 className="card-title"> July 2017 – Sept 2017 </h5>
+                </nav>
+                <ul>
+                    <li>
+                        Created a constraint satisfaction problem solver in Python. The program takes variables and theirs constraints. The program will provide a valid assignment for each of the variables within the given constraints.
+                    </li>
+                    <li>
+                        Implemented a Nonogram puzzle in order to test the performance and correctness of the program. The result of the puzzle would be displayed in a UI using the Tkinter Python API
+                    </li>
+                    <li>
+                        Converted the program into C# and improved memory efficiency using lambda functions for the constraints.
+                    </li>
+                </ul>
+            </div>
+
+            </div >
         );
     }
 }
--- src/scripts/Work.js
+++ src/scripts/Work.js
@@ -2,74 +2,50 @@ import React, { Component } from 'react';
 
 
 class Work extends Component {
-  /**
-   * Class that represents the my Work Experience 
-   */
-  render() {
+    /**
+     * Class that represents the my Work Experience 
+     */
+    render() {
 
-    return (
-  
-        <div className="section"> 
-            <p className="header" id="Work">Work Experience</p>
-            <div className="card card-style" >
-                <nav className="navbar">
-                    <h5 className="card-title">Technical Application Analyst </h5> 
-                    <h5 className="card-title"> Jan 2019 – Current </h5>
-                </nav>
-                <nav className="navbar">
-                    <h10>Tata Consulting Services</h10>
-                </nav>
-                <ul>
-                    <li> 
-                        Placed as TCS contractor to the Royal Bank of Canada to work on their Anti Money Laundering Systems and Fraud prevention 
-                        system to assist with development and support of the systems.
-                    </li>  
-                    <li> 
-                        Monitored the Cloudera Hadoop cluster and resolved any environmental failures and memory issues.
-                        Created SQL queries in DB2 and Hadoop to help find causes of issues. 
-                    </li>
-                    <li>
-                        Created queries to give accurate reports of potential fraudulent transactions or missing transactions to assist with the development of the application.  
-                    </li>
-                    <li> 
-                    Create scripts in order to help automate daily tasks. This includes execution of SQL queries and the creation of Excel spread sheets detailing issues or irregularities in the system.  
-                    </li>
-                    <li> 
-                        Preform system upgrades in order to improve the system performance and to add features to the existing system.  
-                    </li>
-                    <li> 
-                        Technologies: Python, Bash, PowerShell, SQL, Hadoop, DB2
-                    </li>
-                </ul>
+        return (
+
+            <div className="section">
+                <p className="header" id="Work">Work Experience</p>
+                <div className="card card-style" >
+                    <nav className="navbar">
+                        <h5 className="card-title">Technical Application Analyst </h5>
+                        <h5 className="card-title"> Jan 2019 – Current </h5>
+                    </nav>
+                    <nav className="navbar">
+                        <h10>Tata Consulting Services</h10>
+                    </nav>
+                    <ul>
+                        <li> Develop queries to give accurate reports of potential fraudulent transactions or missing transactions to assist with the development of the application.  </li>
+                        <li> Develop scripts in order to help automate daily tasks. This includes execution of SQL queries and the automatic creation of spread sheets detailing issues in the system.  </li>
+                        <li> Collaborate with other teams to organize major upgrades in order to improve the system performance without causing a major downtime to the system. </li>
+                        <li> Support production environment and resolved any failures or memory issues to ensure that the application was preforming as expected. </li>
+                        <li> Technologies: Python, Bash, PowerShell, SQL, Hadoop, DB2 </li>
+                    </ul>
+                </div>
+                <div className="card card-style" >
+                    <nav className="navbar">
+                        <h5 className="card-title"> Project Engineer </h5>
+                        <h5 className="card-title"> June 2018 – Jan 2019 </h5>
+                    </nav>
+                    <nav className="navbar">
+                        <h10> Evertz Microsystems </h10>
+                    </nav>
+                    <ul>
+                        <li> Automated the pre-possessing and transcoding of assets in order for customers to play out videos to their respective users and clients. </li>
+                        <li> Developed the primary scripts for NBCU’s non-linear delivery of assets to media service providers to transfer assets to various locations using different transfer methods, such as Signiant and Faspex.</li>
+                        <li> Refactored SQL queries and reduced run time of existing queries by 50%, in order to improve migration to a new DBMS. </li>
+                        <li> Utilized the Atlassian suite of tools (JIRA, Confluence) to maintain clear lines of communication with team members and project stakeholders, while operating using the SCRUM framework </li>
+                        <li> Technologies: JavaScript, Python, SQL, DB2, MariaDB, Git </li>
+                    </ul>
+                </div>
             </div>
-            <div className="card card-style" >
-                <nav className="navbar">
-                    <h5 className="card-title"> Project Engineer </h5> 
-                    <h5 className="card-title"> June 2018 – Jan 2019 </h5>
-                </nav>
-                <nav className="navbar">
-                    <h10> Evertz Microsystems </h10>
-                </nav>
-                <ul>
-                    <li> 
-                        Created scripts to automate the pre-possessing and transcoding of assets in order for customers to play out videos to their respective users and clients. 
-                    </li> 
-                    <li>
-                        Created the primary scripts for NBCU’s non-linear delivery of assets to media service providers in order to transfer assets to various locations using different transfer methods, such as Signiant and Faspex.
-                    </li>
-                    <li> 
-                        Created targeted SQL queries and reduced run time of existing queries by 50%, in order to improve migration to a new DBMS.                     
-                    </li>
-                    <li> 
-                        Utilized the Atlassian suite of tools (JIRA, Confluence) to maintain clear lines of communication with team members and project stakeholders, while operating using the SCRUM framework                     </li>
-                    <li> 
-                    Technologies: JavaScript, Python, SQL, DB2, MariaDB, Git
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 
