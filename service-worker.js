8e259ccb62b921ba0bfc142f483c09b409aa753a
singha95 <angad88@hotmail.com>
2020-07-06 18:29:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/about-me/precache-manifest.c1898d8f8a7a0b770b67a4989e7930cf.js"
+  "/about-me/precache-manifest.58a44a973de91c1140794ea2ae697010.js"
 );
 
 self.addEventListener('message', (event) => {
